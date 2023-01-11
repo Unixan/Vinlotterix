f04aa368181e1dc7805d52eebc24d4097f8a9621
Unixan <115973884+Unixan@users.noreply.github.com>
2023-01-11 14:50:10 +0100

    It Works! :D

--- JS/trekningView.js
+++ JS/trekningView.js
@@ -5,10 +5,10 @@ function updateTrekningView() {
   model.participants.map(
     (names, index) =>
       (html += /*HTML*/ `
-  <li>${names.name}</li><input type="checkbox" ${
+  <div>${names.name}</li><input type="checkbox" ${
         names.joining ? "checked" : ""
-      } oninput="model.participants[index].joining = !model.participants[index].joining; updateTrekningView()"/>
-  `)
+      } oninput="model.participants[${index}].joining = !model.participants[${index}].joining; updateTrekningView()"/>
+   </div>`)
   );
   document.getElementById("app").innerHTML = html;
 }
