6a4377a304747c33bef77e9d5a2df9ebc2da7cea
Unixan <115973884+Unixan@users.noreply.github.com>
2023-01-11 13:49:28 +0100

    Start

---
+++ JS/model.js
@@ -0,0 +1,3 @@
+const model = {
+  app: { state: "trekningsView" },
+};
---
+++ JS/trekningView.js
@@ -0,0 +1,6 @@
+function updateView() {
+  html = /*HTML*/ `
+    <h1>Vinlotterix</h1>
+  `;
+  document.getElementById("app").innerHTML = html;
+}
