84ee7c4058a83bd105558eb0e221614fecc7800c
Unixan <115973884+Unixan@users.noreply.github.com>
2023-01-12 10:16:25 +0100

    Oppdatert

--- JS/trekningView.js
+++ JS/trekningView.js
@@ -7,7 +7,9 @@ function updateTrekningView() {
       (html += /*HTML*/ `
   <li>${names.name}<input type="checkbox" ${
         names.joining ? "checked" : ""
-      } oninput="model.participants[${index}].joining = !model.participants[${index}].joining; updateTrekningView()"/>
+      } oninput=
+        "model.participants[${index}].joining = !model.participants[${index}].joining;
+         updateTrekningView()"/>
    </li>`)
   );
   document.getElementById("app").innerHTML = html;
